49808593ff06ec1aee82905facca25b0ee6557ae
Lef <ligomenidis@hotmail.com>
2019-01-18 18:16:18 +0200

    Create/Fill Class Order

---
+++ login/login/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace login.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(0, 1000, ErrorMessage = "Cost cannot be negative and must be under 1000 ")]
+        public int TotalCost { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
