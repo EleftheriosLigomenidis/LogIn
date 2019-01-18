03ba0bdc0e8e22785462e3079bc2638aaf416d39
Lef <ligomenidis@hotmail.com>
2019-01-18 18:34:11 +0200

    Add/Fill Class UserRole

---
+++ login/login/Models/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace login.Models
+{
+    public class UserRole
+    {
+        public enum UserRole
+        {
+            Admin,
+            Basic
+        }
+    }
+}
